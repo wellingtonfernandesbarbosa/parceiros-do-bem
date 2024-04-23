16d4e0b79ad13793b679df62bf74a4bafc4b0167
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-04-23 07:03:56 -0300

    Updated contact icons and made some adjustments to responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Logo from "./assets/logo.png";
-import FacebookLogo from "./assets/facebook.svg";
-import InstagramLogo from "./assets/instagram.svg";
+import FacebookLogo from "./assets/facebook.png";
+import InstagramLogo from "./assets/instagram.png";
 import Phone from "./assets/phone.png";
+import Mail from "./assets/mail.png"
 import styles from "./App.module.scss";
 
 function App() {
@@ -15,21 +16,31 @@ function App() {
           <h2 className={styles.Section__Title}>Quem Somos</h2>
           <p className={styles.Section__Description}>Somos uma associação dedicada a promover o bem-estar social e ambiental em nossa comunidade.</p>
         </section>
+
         <section className={styles.Section}>
           <h2 className={styles.Section__Title}>Nossa Missão</h2>
-          <p className={styles.Section__Description}>Buscamos criar um impacto positivo por meio de iniciativas de voluntariado, eventos e projetos sustentáveis.</p>
+          <p className={styles.Section__Description}>Construir um futuro para pessoas que já perderam a esperança, dando um novo rumo para suas histórias.</p>
         </section>
+
+        <section className={styles.Section}>
+          <h2 className={styles.Section__Title}>Nossa Visão</h2>
+          <p className={styles.Section__Description}>Ser referência na cidade de Vespasiano, garantindo um ensino de qualidade e sem destinção a toda a população.</p>
+        </section>
+
         <section className={styles.Section}>
           <h2 className={styles.Section__Title}>Cursos Profissionalizantes</h2>
           <p className={styles.Section__Description}>Oferecemos uma variedade de cursos profissionalizantes para capacitar membros da comunidade e promover o crescimento pessoal e profissional.</p>
         </section>
+
         <section className={styles.Section}>
           <h2 className={styles.Section__Title}>Como Você Pode Ajudar</h2>
           <p className={styles.Section__Description}>Junte-se a nós como voluntário, faça uma doação de produtos ou roupas para nosso bazar beneficente, ou contribua com uma doação financeira através do PIX para ajudar a financiar nossas iniciativas.</p>
           <p className={styles.Section__Description}>
-            Nosso PIX(CNPJ): <span className={styles.Pix} onClick={() => navigator.clipboard.writeText("07608255000180")}>07608255000180</span>
+            Nosso PIX(CNPJ):{" "}
+            <span className={styles.Pix} onClick={() => navigator.clipboard.writeText("07608255000180")}>
+              07608255000180
+            </span>
           </p>
-
         </section>
       </main>
 
@@ -47,10 +58,13 @@ function App() {
             <img src={Phone} alt="Phone" className={styles.Footer__SocialIcon} />
             <span>(31) 98605-0661</span>
           </a>
-          <br />
+          <a href="mailto:parceirosdobemvespasiano@gmail.com" className={styles.Footer__SocialLink}>
+            <img src={Mail} alt="E-mail" className={styles.Footer__SocialIcon} />
+            <span>parceirosdobemvespasiano@gmail.com</span>
+          </a>
         </div>
         <div className={styles.Footer__Location}>
-          <h3 className={styles.Footer__LocationTitle}>Localização</h3>
+          <h3 className={styles.Footer__Location__Title}>Localização</h3>
           <a href="https://www.google.com/maps/place/ONG+Parceiros+do+Bem/@-19.7488302,-43.9529003,15z/data=!4m6!3m5!1s0xa68905c244f699:0x5910ddc10081235a!8m2!3d-19.7488302!4d-43.9529003!16s%2Fg%2F11ss57yqx0?entry=ttu">
             <p className={styles.Footer__LocationDescription}>Rua Santos Dumont 401, Jardim da Gloria, Vespasiano</p>
           </a>
