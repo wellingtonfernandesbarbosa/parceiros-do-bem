2778aa4016e23c3654c5f709d561452f6c741589
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-04-30 13:54:36 -0300

    Header refactored to use managed state

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,31 +5,38 @@ import Email from "../../assets/mail_blue.svg";
 import Phone from "../../assets/phone_blue.svg";
 
 import { Title3 } from "../Typografy";
+import { useParceiroDoBem } from "../../hooks";
 
 const Header = () => {
+  const parceirosDoBem = useParceiroDoBem()[0];
+
   return (
     <header className={styles.Header}>
       <img src={Logo} alt="Logo" className={styles.Header__Logo} />
       <div className={styles.Header__Info}>
         <div className={styles.Header__Info__Email}>
-          <a href="mailto:parceirosdobemvespasiano@gmail.com">
+          <a href={`mailto:${parceirosDoBem.email}`}>
             <img src={Email} alt="Email" className={styles.Header__Info__Icon} />
           </a>
           <div className={styles.Header__Info__Email__Text}>
             <Title3>Email</Title3>
-            <a href="mailto:parceirosdobemvespasiano@gmail.com" title="Email">parceirosdobemvespasiano@gmail.com</a>
+            <a href={`mailto:${parceirosDoBem.email}`} title="Email">
+              {parceirosDoBem.email}
+            </a>
           </div>
         </div>
         <div className={styles.Header__Info__Phone}>
-          <a href="tel:31986050661" title="Ligue para (31) 98605-0661">
-            <img src={Phone} alt="Telefone" className={styles.Header__Info__Icon} />
+          <a href={`tel:${parceirosDoBem.phone.replace("(", "").replace(")", "").replace("-", "").replace(" ", "")}`} title={`Ícone com o link da ONG ${parceirosDoBem.name}`}>
+            <img src={Phone} alt="Ícone de Telefone" className={styles.Header__Info__Icon} />
           </a>
           <div className={styles.Header__Info__Phone__Text}>
             <Title3>Ligue Agora</Title3>
-            <a href="tel:31986050661" title="Ligue para (31) 98605-0661">(31) 98605-0661</a>
+            <a href={`tel:${parceirosDoBem.phone.replace("(", "").replace(")", "").replace("-", "").replace(" ", "")}`} title={`Link com o número da ONG ${parceirosDoBem.name}`}>
+              {parceirosDoBem.phone}
+            </a>
           </div>
         </div>
-        <a href="https://wa.me/5531986050661?text=Olá! Gostaria de fazer uma doação para ajudar a ONG Parceiros do Bem." target="_blank" title="Doar" className={styles.Header__Info__Donate}>
+        <a href={`${parceirosDoBem.whatsApp}?text=Olá! Gostaria de fazer uma doação para ajudar a ONG Parceiros do Bem.`} target="_blank" title="Doar" className={styles.Header__Info__Donate}>
           <button className={styles.Header__Info__Donate__Button}>Doar</button>
         </a>
       </div>
