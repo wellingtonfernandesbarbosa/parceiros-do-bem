435c5a2360432f70cae14f014c844b3e3cfe9943
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-04-29 21:07:08 -0300

    Implemented social media field in the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,6 +4,10 @@ import Logo from "../../assets/logo.png";
 import locationLogo from "../../assets/location-white.png";
 import phoneLogo from "../../assets/phone-white.png";
 import mailIcon from "../../assets/mail-white.png";
+import WhatsappLogo from "../../assets/whatsapp-white.png";
+import FacebookWhiteLogo from "../../assets/facebook-white.svg";
+import InstagramLogo from "../../assets/instagram-white.png";
+import YoutubeWhiteIcon from "../../assets/youtube-white.png";
 import NavBar from "../NavBar";
 
 const Footer = () => {
@@ -12,7 +16,7 @@ const Footer = () => {
       <div className={styles.Footer__Container}>
         <div className={styles.Footer__Container__Info}>
           <img src={Logo} alt="Logo" title="Parceiros do Bem" className={styles.Footer__Container__Info__Logo} />
-          <p className={styles.Footer__Container__Info__Text}>Transformando Vidas Através do Desenvolvimento Humano.</p>
+          <p className={styles.Footer__Container__Info__Text}>Transformando Vidas Através do Desenvolvimento Humano</p>
           <ul className={styles.Footer__Container__Info__List}>
             <li className={styles.Footer__Container__Info__List__Item}>
               <a href="https://www.google.com/maps/place/ONG+Parceiros+do+Bem/@-19.7488302,-43.9529003,15z/data=!4m6!3m5!1s0xa68905c244f699:0x5910ddc10081235a!8m2!3d-19.7488302!4d-43.9529003!16s%2Fg%2F11ss57yqx0?entry=ttu" title="Endereço" target="_blank">
@@ -38,7 +42,35 @@ const Footer = () => {
           <h2>Páginas</h2>
           <NavBar />
         </div>
-        <div className={styles.Footer__Container__Social}></div>
+        <div className={styles.Footer__Container__Social}>
+          <h2>Redes Sociais</h2>
+          <ul className={styles.Footer__Container__Social__Icons}>
+            <li className={styles.Footer__Container__Social__Icons__Item}>
+              <a href="https://wa.me/5531986050661" target="_blank" rel="noopener noreferrer" title="WhatsApp">
+                <img src={WhatsappLogo} alt="WhatsApp" className={styles.Footer__SocialIcon} />
+                <span>WhatsApp</span>
+              </a>
+            </li>
+            <li className={styles.Footer__Container__Social__Icons__Item}>
+              <a href="https://www.facebook.com/people/Ong-Parceiros-do-Bem/100083482865397/" target="_blank" rel="noopener noreferrer" title="Facebook">
+                <img src={FacebookWhiteLogo} alt="Facebook" className={styles.Footer__SocialLink__SocialIcon} />
+                <span>Facebook</span>
+              </a>
+            </li>
+            <li className={styles.Footer__Container__Social__Icons__Item}>
+              <a href="https://www.instagram.com/ongparceirosdobem_" target="_blank" rel="noopener noreferrer" title="Instagram">
+                <img src={InstagramLogo} alt="Instagram" className={styles.Footer__SocialLink__SocialIcon} />
+                <span>Instagram</span>
+              </a>
+            </li>
+            <li className={styles.Footer__Container__Social__Icons__Item}>
+              <a href="https://www.youtube.com/channel/UCxNhCpGKVcch10CrhgvOyWg" target="_blank" rel="noopener noreferrer" title="Youtube" >
+                <img src={YoutubeWhiteIcon} alt="Youtube" className={styles.Footer__SocialLink__SocialIcon} />
+                <span>Youtube</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
