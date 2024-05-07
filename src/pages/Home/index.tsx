1a71f5f6548bd9e406425cdd2566f13155dbb84c
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-05-07 20:13:31 -0300

    Added call-to-action cards on the homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
 import Banner from "../../components/Banner";
+import CallToAction from "../../components/CallToAction";
 
 const Home = () => {
   return <>
     <Banner />
+    <CallToAction />
   </>;
 };
 
