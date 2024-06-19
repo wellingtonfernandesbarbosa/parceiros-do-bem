c3e6cb8d39e183999ae04d48378ce6a647bb8634
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-06-19 15:58:18 -0300

    Added header to classes page

--- src/pages/ClassesPage/index.tsx
+++ src/pages/ClassesPage/index.tsx
@@ -3,12 +3,16 @@ import styles from "./ClassesPage.module.scss";
 import { Link } from "react-router-dom";
 
 import lessons from "@/data/classes.json";
-import { Paragraph, Title3 } from "@/components/Typografy";
+import { Paragraph, Title2, Title3 } from "@/components/Typografy";
 
 const ClassesPage = () => {
   return (
     <main className={styles.ClassesPage}>
       <div className={styles.ClassesPage__container}>
+        <header className={styles.ClassesPage__container__header}>
+          <Title2>Explore nossas aulas para aprimorar suas habilidades e interesses</Title2>
+          <Paragraph>Oferecemos uma ampla variedade de aulas gratuitas que abrangem desde desenvolvimento profissional até hobbies e interesses pessoais. Descubra novas paixões e aprenda com nossos instrutores experientes.</Paragraph>
+        </header>
         <ul className={styles.ClassesPage__container__list}>
           {lessons.map((lesson) => (
             <li key={lesson.id} className={styles.ClassesPage__container__list__item}>
