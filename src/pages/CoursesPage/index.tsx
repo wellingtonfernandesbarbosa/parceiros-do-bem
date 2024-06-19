6f9296f6592f3f70da0419b8eacce8decde8582f
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-06-19 15:51:16 -0300

    Added header to courses page

--- src/pages/CoursesPage/index.tsx
+++ src/pages/CoursesPage/index.tsx
@@ -4,7 +4,7 @@ import { Link, useParams } from "react-router-dom";
 
 import courses from "@/data/courses.json";
 import ScrollToTop from "@/util/scrollToTop";
-import { Paragraph, Title3 } from "@/components/Typografy";
+import { Paragraph, Title2, Title3 } from "@/components/Typografy";
 
 const CoursesPage = () => {
   const param = Number(useParams().id);
@@ -13,6 +13,13 @@ const CoursesPage = () => {
   return (
     <main className={styles.Courses}>
       <div className={styles.Courses__container}>
+        <header className={styles.Courses__container__header}>
+          <Title2>Explore Nossos Cursos de Capacitação Profissional</Title2>
+          <Paragraph>
+            Oferecemos uma variedade de cursos projetados para desenvolver suas habilidades e prepará-lo para o mercado de trabalho. Desde cuidados pessoais até habilidades técnicas, nossos cursos são ministrados por profissionais experientes e estão
+            disponíveis a preços acessíveis. Encontre o curso ideal para você e comece sua jornada de aprendizado hoje mesmo!
+          </Paragraph>
+        </header>
         <ul className={styles.Courses__container__list}>
           {courses.map((course) => (
             <li key={course.id} className={styles.Courses__container__list__item}>
